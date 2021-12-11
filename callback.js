eff9e7f1e92629d0938c7e5527c60b787c33f602
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2021-12-11 08:24:51 -0800

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,18 @@
+// function fn(x,mn)
+// {
+//     console.log("HI")
+//     console.log(x)
+//     mn()
+// }
+// function fn1()
+// {
+//     console.log("hello")
+// }
+// fn("mabu",fn1)
+
+
+var c=function(){
+    console.log("ananymous function")
+
+}
+c()
